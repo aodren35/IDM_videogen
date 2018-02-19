5b02a989d87d8263ed0d7d69f94a2ee75401a29c
aodren35 <aodren.letellier@etudiant.univ-rennes1.fr>
2018-02-19 01:47:55 +0100

    TP3 q1

--- VideoGenToolSuite/bin/VideoGen.xtend
+++ VideoGenToolSuite/bin/VideoGen.xtend
@@ -9,17 +9,33 @@ import org.xtext.example.mydsl.videoGen.*
 import utils.Randomiser
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.List
+import java.util.ArrayList
+import java.io.PrintWriter
+import java.io.File
+import java.io.FileNotFoundException
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.Files
+import java.io.FileWriter
 
 class VideoGen {
 	
+	private static VideoGeneratorModel videoGenUpdated
+	private static List<VideoDescription> allVideos = new ArrayList<VideoDescription>()
+	
 	def static void main(String[] args) {
-    	var videogen = generate("example1.videogen");
+    	var videogen = generate("example1.videogen")
     	writeInFile("playlist.txt", videogen)
     	generateVideo()
     	generateThumbnails("example1.videogen")
     	println("getlongestvar " + getLongestVar("example1.videogen") )
+    	writeStatsToCsv()
+    	// new VideoGenHelper().saveVideoGenerator(URI.createURI("example1.videogen"), videoGenUpdated)
   	}
 	
+	
+	// TODO : ajouter duration si absentes
 	static def String generate(String uri) {
 		val videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI(uri))
 		// and then visit the model
@@ -32,7 +48,7 @@ class VideoGen {
 				
 				if (video instanceof MandatoryVideoSeq) {
 					val desc =  video.description
-					playlist.add("file '" + desc.location + "'")		
+					playlist.add("file '" + desc.location + "'")
 						
 				}
 				if (video instanceof OptionalVideoSeq) {
@@ -59,6 +75,7 @@ class VideoGen {
 		for (String pl : playlist)
 			playlistStr += pl + "\n"
 		
+		videoGenUpdated = videoGen
 		return playlistStr
 		
 		
@@ -82,22 +99,29 @@ class VideoGen {
 				
 				if (video instanceof MandatoryVideoSeq ) {
 					val desc =  video.description
+					video.description.duration = readDuration(desc.location)
 					generateThumbnail(desc.videoid, desc.location)
+					allVideos.add(desc)		
 						
 				}
 				if (video instanceof OptionalVideoSeq) {
 					val desc =  video.description
+					video.description.duration = readDuration(desc.location)
 					generateThumbnail(desc.videoid, desc.location)
+					allVideos.add(desc)		
 				}
 				if (video instanceof AlternativeVideoSeq) {
 					val alts = (video as AlternativeVideoSeq)
 					for (VideoDescription videodesc: alts.videodescs) {
 						val desc = videodesc
+						videodesc.duration = readDuration(desc.location)
 						generateThumbnail(desc.videoid, desc.location)
+						allVideos.add(desc)		
 					}
 				}	
 			}
 		]
+		videoGenUpdated = videoGen
 	}
 	
 	
@@ -175,7 +199,11 @@ class VideoGen {
 	
 	static def int readDuration(String locationVideo) {
 			
-			var p = Runtime.getRuntime().exec(ffmpegComputeDuration(locationVideo).toString);
+			var Process p
+			var ffmpegCmd = ffmpegComputeDuration(locationVideo).toString 
+			println(ffmpegCmd)
+		
+			p = Runtime.getRuntime().exec(ffmpegCmd);
 			p.waitFor();
 			
 			var BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -188,5 +216,107 @@ class VideoGen {
 		    return Math.round(Float.parseFloat(lines))-1;
 	}
 	
-	
+	static def List<List<VideoDescription>> generateAllVars() {
+		
+		var List<List<VideoDescription>> result = new ArrayList<List<VideoDescription>>()
+		var List<VideoDescription> lempty = new ArrayList<VideoDescription>()
+		result.add(lempty)
+		for (Media media : videoGenUpdated.medias)	{
+			if (media instanceof VideoSeq) {			
+				val video = (media as VideoSeq)
+				
+				if (video instanceof MandatoryVideoSeq ) {
+					val desc =  video.description
+					for (List<VideoDescription> l: result){
+						l.add(desc)
+					}	
+				}
+				if (video instanceof OptionalVideoSeq) {
+					val desc =  video.description	
+						
+					var partial = new ArrayList<List<VideoDescription>>()
+					for(List<VideoDescription>l : result) {
+						var lTemp = new ArrayList<VideoDescription>()
+						lTemp.addAll(l)
+						partial.add(lTemp)
+						l.add(desc)
+					}
+					result.addAll(partial)
+					
+				}
+				if (video instanceof AlternativeVideoSeq) {
+					val alts = (video as AlternativeVideoSeq)
+					var partial = new ArrayList<List<VideoDescription>>()
+					for (var it = result.iterator(); it.hasNext();){
+						var l = it.next()
+						it.remove()
+						for (VideoDescription videodesc: alts.videodescs) {
+							val desc = videodesc
+							var lTemp = new ArrayList<VideoDescription>()
+							lTemp.addAll(l)
+							lTemp.add(desc)
+							partial.add(lTemp)
+						}
+					}
+					result.addAll(partial)
+				}	
+			}
+		}
+		return result
+	}
+
+	static def void writeStatsToCsv() {
+		var PrintWriter pw = null
+		var File file = new File("C:/Users/aodre/Documents/Cours/M2/IDM/IDM_videogen/VideoGenToolSuite/ressources/stats/vars_size.csv")
+		var fl = new FileWriter(file);
+		
+     
+        fl.append("id")
+        fl.append(",")
+        
+        for(VideoDescription desc: allVideos) {
+            fl.append(desc.videoid)
+            fl.append(",")
+        }
+        
+        fl.append("size")
+        fl.append("\n")
+		
+        var index = 1;
+		for(List<VideoDescription> v : generateAllVars()) {
+			var size = 0
+			fl.append(index + "")
+			fl.append(",")
+			
+			for(VideoDescription desc : allVideos) {
+				if(hasVideo(v, desc)) {
+					fl.append("TRUE")
+					
+					var path = Paths.get(desc.location)
+					var byte[] data = Files.readAllBytes(path)
+					
+					size += data.length
+				} else {
+					fl.append("FALSE")
+				}
+				fl.append(",")
+			}
+			
+			fl.append(size +"")
+			fl.append("\n")
+			
+			index++
+		}
+		fl.flush()
+		fl.close()
+	}
+
+	static def boolean hasVideo(List<VideoDescription> l, VideoDescription v){
+		for (VideoDescription vTemp: l) {
+			if (vTemp.videoid == v.videoid) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- VideoGenToolSuite/xtend-gen/VideoGen.java
+++ VideoGenToolSuite/xtend-gen/VideoGen.java
@@ -1,12 +1,20 @@
 import com.google.common.base.Objects;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.function.Consumer;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
@@ -25,6 +33,10 @@ import utils.Randomiser;
 
 @SuppressWarnings("all")
 public class VideoGen {
+  private static VideoGeneratorModel videoGenUpdated;
+  
+  private static List<VideoDescription> allVideos = new ArrayList<VideoDescription>();
+  
   public static void main(final String[] args) {
     String videogen = VideoGen.generate("example1.videogen");
     VideoGen.writeInFile("playlist.txt", videogen);
@@ -33,6 +45,7 @@ public class VideoGen {
     int _longestVar = VideoGen.getLongestVar("example1.videogen");
     String _plus = ("getlongestvar " + Integer.valueOf(_longestVar));
     InputOutput.<String>println(_plus);
+    VideoGen.writeStatsToCsv();
   }
   
   public static String generate(final String uri) {
@@ -80,6 +93,7 @@ public class VideoGen {
       String _playlistStr = playlistStr;
       playlistStr = (_playlistStr + (pl + "\n"));
     }
+    VideoGen.videoGenUpdated = videoGen;
     return playlistStr;
   }
   
@@ -100,11 +114,17 @@ public class VideoGen {
         final VideoSeq video = ((VideoSeq) media);
         if ((video instanceof MandatoryVideoSeq)) {
           final VideoDescription desc = ((MandatoryVideoSeq)video).getDescription();
+          VideoDescription _description = ((MandatoryVideoSeq)video).getDescription();
+          _description.setDuration(VideoGen.readDuration(desc.getLocation()));
           VideoGen.generateThumbnail(desc.getVideoid(), desc.getLocation());
+          VideoGen.allVideos.add(desc);
         }
         if ((video instanceof OptionalVideoSeq)) {
           final VideoDescription desc_1 = ((OptionalVideoSeq)video).getDescription();
+          VideoDescription _description_1 = ((OptionalVideoSeq)video).getDescription();
+          _description_1.setDuration(VideoGen.readDuration(desc_1.getLocation()));
           VideoGen.generateThumbnail(desc_1.getVideoid(), desc_1.getLocation());
+          VideoGen.allVideos.add(desc_1);
         }
         if ((video instanceof AlternativeVideoSeq)) {
           final AlternativeVideoSeq alts = ((AlternativeVideoSeq) video);
@@ -112,13 +132,16 @@ public class VideoGen {
           for (final VideoDescription videodesc : _videodescs) {
             {
               final VideoDescription desc_2 = videodesc;
+              videodesc.setDuration(VideoGen.readDuration(desc_2.getLocation()));
               VideoGen.generateThumbnail(desc_2.getVideoid(), desc_2.getLocation());
+              VideoGen.allVideos.add(desc_2);
             }
           }
         }
       }
     };
     videoGen.getMedias().forEach(_function);
+    VideoGen.videoGenUpdated = videoGen;
   }
   
   public static int getLongestVar(final String uri) {
@@ -218,7 +241,10 @@ public class VideoGen {
   
   public static int readDuration(final String locationVideo) {
     try {
-      Process p = Runtime.getRuntime().exec(VideoGen.ffmpegComputeDuration(locationVideo).toString());
+      Process p = null;
+      String ffmpegCmd = VideoGen.ffmpegComputeDuration(locationVideo).toString();
+      InputOutput.<String>println(ffmpegCmd);
+      p = Runtime.getRuntime().exec(ffmpegCmd);
       p.waitFor();
       InputStream _inputStream = p.getInputStream();
       InputStreamReader _inputStreamReader = new InputStreamReader(_inputStream);
@@ -233,5 +259,122 @@ public class VideoGen {
     } catch (Throwable _e) {
       throw Exceptions.sneakyThrow(_e);
     }
+  }
+  
+  public static List<List<VideoDescription>> generateAllVars() {
+    List<List<VideoDescription>> result = new ArrayList<List<VideoDescription>>();
+    List<VideoDescription> lempty = new ArrayList<VideoDescription>();
+    result.add(lempty);
+    EList<Media> _medias = VideoGen.videoGenUpdated.getMedias();
+    for (final Media media : _medias) {
+      if ((media instanceof VideoSeq)) {
+        final VideoSeq video = ((VideoSeq) media);
+        if ((video instanceof MandatoryVideoSeq)) {
+          final VideoDescription desc = ((MandatoryVideoSeq)video).getDescription();
+          for (final List<VideoDescription> l : result) {
+            l.add(desc);
+          }
+        }
+        if ((video instanceof OptionalVideoSeq)) {
+          final VideoDescription desc_1 = ((OptionalVideoSeq)video).getDescription();
+          ArrayList<List<VideoDescription>> partial = new ArrayList<List<VideoDescription>>();
+          for (final List<VideoDescription> l_1 : result) {
+            {
+              ArrayList<VideoDescription> lTemp = new ArrayList<VideoDescription>();
+              lTemp.addAll(l_1);
+              partial.add(lTemp);
+              l_1.add(desc_1);
+            }
+          }
+          result.addAll(partial);
+        }
+        if ((video instanceof AlternativeVideoSeq)) {
+          final AlternativeVideoSeq alts = ((AlternativeVideoSeq) video);
+          ArrayList<List<VideoDescription>> partial_1 = new ArrayList<List<VideoDescription>>();
+          for (Iterator<List<VideoDescription>> it = result.iterator(); it.hasNext();) {
+            {
+              List<VideoDescription> l_2 = it.next();
+              it.remove();
+              EList<VideoDescription> _videodescs = alts.getVideodescs();
+              for (final VideoDescription videodesc : _videodescs) {
+                {
+                  final VideoDescription desc_2 = videodesc;
+                  ArrayList<VideoDescription> lTemp = new ArrayList<VideoDescription>();
+                  lTemp.addAll(l_2);
+                  lTemp.add(desc_2);
+                  partial_1.add(lTemp);
+                }
+              }
+            }
+          }
+          result.addAll(partial_1);
+        }
+      }
+    }
+    return result;
+  }
+  
+  public static void writeStatsToCsv() {
+    try {
+      PrintWriter pw = null;
+      File file = new File("C:/Users/aodre/Documents/Cours/M2/IDM/IDM_videogen/VideoGenToolSuite/ressources/stats/vars_size.csv");
+      FileWriter fl = new FileWriter(file);
+      fl.append("id");
+      fl.append(",");
+      for (final VideoDescription desc : VideoGen.allVideos) {
+        {
+          fl.append(desc.getVideoid());
+          fl.append(",");
+        }
+      }
+      fl.append("size");
+      fl.append("\n");
+      int index = 1;
+      List<List<VideoDescription>> _generateAllVars = VideoGen.generateAllVars();
+      for (final List<VideoDescription> v : _generateAllVars) {
+        {
+          int size = 0;
+          String _plus = (Integer.valueOf(index) + "");
+          fl.append(_plus);
+          fl.append(",");
+          for (final VideoDescription desc_1 : VideoGen.allVideos) {
+            {
+              boolean _hasVideo = VideoGen.hasVideo(v, desc_1);
+              if (_hasVideo) {
+                fl.append("TRUE");
+                Path path = Paths.get(desc_1.getLocation());
+                byte[] data = Files.readAllBytes(path);
+                int _size = size;
+                int _length = data.length;
+                size = (_size + _length);
+              } else {
+                fl.append("FALSE");
+              }
+              fl.append(",");
+            }
+          }
+          String _plus_1 = (Integer.valueOf(size) + "");
+          fl.append(_plus_1);
+          fl.append("\n");
+          index++;
+        }
+      }
+      fl.flush();
+      fl.close();
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  public static boolean hasVideo(final List<VideoDescription> l, final VideoDescription v) {
+    for (final VideoDescription vTemp : l) {
+      String _videoid = vTemp.getVideoid();
+      String _videoid_1 = v.getVideoid();
+      boolean _equals = Objects.equal(_videoid, _videoid_1);
+      if (_equals) {
+        return true;
+      }
+    }
+    return false;
   }
 }
