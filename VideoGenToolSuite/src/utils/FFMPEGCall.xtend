a7f7ec5f56fa6c20af07a715e1387faf8d933834
aodren35 <aodren.letellier@etudiant.univ-rennes1.fr>
2018-02-22 23:07:26 +0100

    Some refractor

---
+++ VideoGenToolSuite/src/utils/FFMPEGCall.xtend
@@ -0,0 +1,191 @@
+package utils
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.FileOutputStream
+import org.xtext.example.mydsl.videoGen.VideoDescription
+
+public class FFMPEGCall {
+	
+	private final static String PATH_TOOL = "C:/Users/aodre/Documents/Cours/M2/IDM/IDM_videogen/VideoGenToolSuite/"
+	private final static String PATH_GEN_RELATIVE = "ressources/gen/"
+	private final static String PATH_GEN_VIGNETTES_RELATIVE = "ressources/gen/vignettes/"
+	private final static String PATH_RESSOURCES = "ressources/"
+	
+	private String tag = ""
+	
+	new (String tag) {
+		this.tag = tag
+	}
+	
+	def int launchFfmpegCmd(String ffmpegCmd) {
+		println(ffmpegCmd)
+		var Process p 
+		var FileOutputStream fos = new FileOutputStream('logger.txt')
+	 	p = Runtime.runtime.exec(ffmpegCmd)
+	 	// error
+        var StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(), "ERROR")           
+		// output
+        var StreamGobbler outputGobbler = new StreamGobbler(p.getInputStream(), "OUTPUT", fos)   
+        // vide les streams
+        errorGobbler.start()
+        outputGobbler.start()
+        var int exitVal = p.waitFor
+        fos.flush()
+        fos.close()
+        return exitVal
+	}
+	
+	def void generateVideo(String source, String target) {
+		var ffmpegCmd = ffmpegConcatenateCommand(PATH_TOOL + source, PATH_GEN_RELATIVE + target).toString 
+		launchFfmpegCmd(ffmpegCmd)
+	}
+	
+	def applyFilterFilpH(String string) {
+		var ffmpegCmd = ffmpegFlipH(PATH_TOOL + string, PATH_GEN_RELATIVE + "filtered_"+string).toString 
+		launchFfmpegCmd(ffmpegCmd)
+	}
+	
+	def applyFilterFilpV(String string) {
+		var ffmpegCmd = ffmpegFlipV(PATH_TOOL +PATH_GEN_RELATIVE+ string, PATH_GEN_RELATIVE + "filtered_"+string).toString 
+		launchFfmpegCmd(ffmpegCmd)
+	}
+	
+	def applyFilterNegate(String string) {
+		var ffmpegCmd = ffmpegNegate(PATH_TOOL +PATH_GEN_RELATIVE+ string, PATH_GEN_RELATIVE + "filtered_"+string).toString 
+		launchFfmpegCmd(ffmpegCmd)
+	}
+	
+	def applyFilterBN(String string) {
+		var ffmpegCmd = ffmpegBN(PATH_TOOL +PATH_GEN_RELATIVE+ string, PATH_GEN_RELATIVE + "filtered_"+string).toString 
+		launchFfmpegCmd(ffmpegCmd)
+	}	
+	
+	def void generateVideoFilteredWithText(VideoDescription desc, String source, String target) {
+		var Process p 
+		var text = desc.text.content
+		var x = 50
+		var y = 0
+		var color = desc.text.color
+		var size = desc.text.size
+		var String ffmpegCmd
+		if (size == 0) { 
+			size = 20
+		}
+		switch desc.text.position {
+  			case 'TOP' : {
+  				ffmpegCmd = ffmpegDrawTextTOP(PATH_TOOL + source, PATH_GEN_RELATIVE + target, text,  color, size).toString 
+  			}
+  			case 'BOTTOM' : {
+  				ffmpegCmd = ffmpegDrawTextBOTTOM(PATH_TOOL + source, PATH_GEN_RELATIVE + target, text, color, size).toString 
+  			}
+  			case 'CENTER': {
+  				ffmpegCmd = ffmpegDrawTextCENTER(PATH_TOOL + source, PATH_GEN_RELATIVE + target, text, color, size).toString 
+  			}
+  			default : ffmpegCmd = ffmpegDrawTextCENTER(PATH_TOOL + source, PATH_GEN_RELATIVE + target, text, color, size).toString 
+		}
+		
+		launchFfmpegCmd(ffmpegCmd)
+		    
+	}
+	
+	def void generateGif(String source, String target, int t, int w, int l) {
+		var Process p
+		var int time = 0
+		var int width = 320
+		var int length = 240
+		if (t != -1) time = t
+		if (w != -1) width = w
+		if (l != -1) length = l
+		var ffmpegCmd = ffmpegVideoToGif(PATH_TOOL+ PATH_GEN_RELATIVE + source, PATH_TOOL+PATH_GEN_RELATIVE + target, time, width, length).toString 
+		launchFfmpegCmd(ffmpegCmd)
+	}
+	
+	// Différent car on attend un résultat de l'execution de la commande
+	def int videoDuration(String locationVideo) {
+			
+			var Process p
+			var ffmpegCmd = ffmpegComputeDuration(locationVideo).toString 
+		
+			p = Runtime.getRuntime().exec(ffmpegCmd);
+			p.waitFor();
+			
+			var BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			var String line = "";
+			var String lines = "";
+		    while ((line = reader.readLine()) != null) {
+		        lines = lines + line;
+		    }
+		    return Math.round(Float.parseFloat(lines))-1;
+	}
+	
+	def void generateThumbnail(int id, String loc) {
+		var ffmpegCmd = ffmpegThumbnail(id, loc).toString 
+		launchFfmpegCmd(ffmpegCmd)
+	}	
+	
+	def ffmpegThumbnail (int id, String loc) '''
+		ffmpeg -i «loc» -ss 00:00:01.000 -vframes 1 «PATH_GEN_VIGNETTES_RELATIVE+tag+"_"+id».jpg -y
+	'''
+ 	
+	def ffmpegFlipH(String inputPath, String outputPath) '''
+		ffmpeg -i «inputPath» -vf "hflip" «outputPath» -y
+	'''
+	
+	def ffmpegFlipV(String inputPath, String outputPath) '''
+		ffmpeg -i «inputPath» -vf "vflip" «outputPath» -y
+	'''
+	
+	def ffmpegNegate(String inputPath, String outputPath) '''
+		ffmpeg -i «inputPath» -vf "negate" «outputPath» -y
+	'''
+	
+	def ffmpegBN(String inputPath, String outputPath) '''
+		ffmpeg -i «inputPath» -vf "hue=s=0" «outputPath» -y
+	'''
+
+	def ffmpegDrawTextTOP(String inputPath, String outputPath, String text,   String color, int size) '''
+		ffmpeg -i «inputPath» -vf "drawtext=fontfile='C\:\\Windows\\fonts\\Arial.ttf':text=«text»:x=(w-text_w)/2:fontsize=«size»:fontcolor=«color»" «outputPath» -y
+	'''
+	
+	def ffmpegDrawTextCENTER(String inputPath, String outputPath, String text,  String color, int size) '''
+		ffmpeg -i «inputPath» -vf "drawtext=fontfile='C\:\\Windows\\fonts\\Arial.ttf':text=«text»:x=(w-text_w)/2:y=(h-text_h)/2:fontsize=«size»:fontcolor=«color»" «outputPath» -y
+	'''
+	def ffmpegDrawTextBOTTOM(String inputPath, String outputPath, String text, String color, int size) '''
+		ffmpeg -i «inputPath» -vf "drawtext=fontfile='C\:\\Windows\\fonts\\Arial.ttf':text=«text»:x=(w-text_w)/2:y=(h-text_h):fontsize=«size»:fontcolor=«color»" «outputPath» -y
+	'''
+
+	def ffmpegConcatenateCommand(String mpegPlaylistFile, String outputPath) '''
+			 ffmpeg -y -f concat -safe 0 -i «mpegPlaylistFile» -c copy -r 24 «outputPath»
+		'''
+		
+	def ffmpegComputeDuration(String locationVideo) '''
+		ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 «locationVideo»
+	'''
+	
+//	def ffmpegVideoToPalette(String videoLocation) '''
+//		 ffmpeg -v warning -ss  0 -i «videoLocation» -vf "fps=15,scale=-1:-1:flags=lanczos,palettegen" -y «PATH_TEMP_PALETTE»
+//	'''
+//	def ffmpegPaletteToGif(String videoLocation, String outputPath) 	/*'''
+//		 ffmpeg -i «videoLocation» -ss 00:00:00.000 -pix_fmt rgb24 -r 10 -s 320x240   «outputPath»
+//	'''*/
+//	'''
+//		 ffmpeg -v warning -ss 0 -i «videoLocation» -i «PATH_TEMP_PALETTE» -lavfi "fps=15,scale=-1:-1:flags=lanczos, [x]; [x][1:v] paletteuse" -y  «outputPath»
+//	'''
+
+
+	def String ffmpegVideoToGif(String videoLocation, String outputPath, int time, int w, int l){
+		if (time != 0) { 
+			return 
+				''' 
+		 		ffmpeg -i «videoLocation» -y -ss 0 -pix_fmt rgb8 -r 10 -t «time» -s «w»x«l»  «outputPath»
+				'''
+		} else {
+			return 
+				''' 
+				 ffmpeg -i «videoLocation» -y -ss 0 -pix_fmt rgb8 -r 10 -s «w»x«l»  «outputPath»
+				'''
+		}
+	}
+}
---
+++ VideoGenToolSuite/xtend-gen/utils/FFMPEGCall.java
@@ -0,0 +1,322 @@
+package utils;
+
+import com.google.common.base.Objects;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+import org.xtext.example.mydsl.videoGen.VideoDescription;
+import utils.StreamGobbler;
+
+@SuppressWarnings("all")
+public class FFMPEGCall {
+  private final static String PATH_TOOL = "C:/Users/aodre/Documents/Cours/M2/IDM/IDM_videogen/VideoGenToolSuite/";
+  
+  private final static String PATH_GEN_RELATIVE = "ressources/gen/";
+  
+  private final static String PATH_GEN_VIGNETTES_RELATIVE = "ressources/gen/vignettes/";
+  
+  private final static String PATH_RESSOURCES = "ressources/";
+  
+  private String tag = "";
+  
+  public FFMPEGCall(final String tag) {
+    this.tag = tag;
+  }
+  
+  public int launchFfmpegCmd(final String ffmpegCmd) {
+    try {
+      InputOutput.<String>println(ffmpegCmd);
+      Process p = null;
+      FileOutputStream fos = new FileOutputStream("logger.txt");
+      p = Runtime.getRuntime().exec(ffmpegCmd);
+      InputStream _errorStream = p.getErrorStream();
+      StreamGobbler errorGobbler = new StreamGobbler(_errorStream, "ERROR");
+      InputStream _inputStream = p.getInputStream();
+      StreamGobbler outputGobbler = new StreamGobbler(_inputStream, "OUTPUT", fos);
+      errorGobbler.start();
+      outputGobbler.start();
+      int exitVal = p.waitFor();
+      fos.flush();
+      fos.close();
+      return exitVal;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  public void generateVideo(final String source, final String target) {
+    String ffmpegCmd = this.ffmpegConcatenateCommand((FFMPEGCall.PATH_TOOL + source), (FFMPEGCall.PATH_GEN_RELATIVE + target)).toString();
+    this.launchFfmpegCmd(ffmpegCmd);
+  }
+  
+  public int applyFilterFilpH(final String string) {
+    int _xblockexpression = (int) 0;
+    {
+      String ffmpegCmd = this.ffmpegFlipH((FFMPEGCall.PATH_TOOL + string), ((FFMPEGCall.PATH_GEN_RELATIVE + "filtered_") + string)).toString();
+      _xblockexpression = this.launchFfmpegCmd(ffmpegCmd);
+    }
+    return _xblockexpression;
+  }
+  
+  public int applyFilterFilpV(final String string) {
+    int _xblockexpression = (int) 0;
+    {
+      String ffmpegCmd = this.ffmpegFlipV(((FFMPEGCall.PATH_TOOL + FFMPEGCall.PATH_GEN_RELATIVE) + string), ((FFMPEGCall.PATH_GEN_RELATIVE + "filtered_") + string)).toString();
+      _xblockexpression = this.launchFfmpegCmd(ffmpegCmd);
+    }
+    return _xblockexpression;
+  }
+  
+  public int applyFilterNegate(final String string) {
+    int _xblockexpression = (int) 0;
+    {
+      String ffmpegCmd = this.ffmpegNegate(((FFMPEGCall.PATH_TOOL + FFMPEGCall.PATH_GEN_RELATIVE) + string), ((FFMPEGCall.PATH_GEN_RELATIVE + "filtered_") + string)).toString();
+      _xblockexpression = this.launchFfmpegCmd(ffmpegCmd);
+    }
+    return _xblockexpression;
+  }
+  
+  public int applyFilterBN(final String string) {
+    int _xblockexpression = (int) 0;
+    {
+      String ffmpegCmd = this.ffmpegBN(((FFMPEGCall.PATH_TOOL + FFMPEGCall.PATH_GEN_RELATIVE) + string), ((FFMPEGCall.PATH_GEN_RELATIVE + "filtered_") + string)).toString();
+      _xblockexpression = this.launchFfmpegCmd(ffmpegCmd);
+    }
+    return _xblockexpression;
+  }
+  
+  public void generateVideoFilteredWithText(final VideoDescription desc, final String source, final String target) {
+    Process p = null;
+    String text = desc.getText().getContent();
+    int x = 50;
+    int y = 0;
+    String color = desc.getText().getColor();
+    int size = desc.getText().getSize();
+    String ffmpegCmd = null;
+    if ((size == 0)) {
+      size = 20;
+    }
+    String _position = desc.getText().getPosition();
+    if (_position != null) {
+      switch (_position) {
+        case "TOP":
+          ffmpegCmd = this.ffmpegDrawTextTOP((FFMPEGCall.PATH_TOOL + source), (FFMPEGCall.PATH_GEN_RELATIVE + target), text, color, size).toString();
+          break;
+        case "BOTTOM":
+          ffmpegCmd = this.ffmpegDrawTextBOTTOM((FFMPEGCall.PATH_TOOL + source), (FFMPEGCall.PATH_GEN_RELATIVE + target), text, color, size).toString();
+          break;
+        case "CENTER":
+          ffmpegCmd = this.ffmpegDrawTextCENTER((FFMPEGCall.PATH_TOOL + source), (FFMPEGCall.PATH_GEN_RELATIVE + target), text, color, size).toString();
+          break;
+        default:
+          ffmpegCmd = this.ffmpegDrawTextCENTER((FFMPEGCall.PATH_TOOL + source), (FFMPEGCall.PATH_GEN_RELATIVE + target), text, color, size).toString();
+          break;
+      }
+    } else {
+      ffmpegCmd = this.ffmpegDrawTextCENTER((FFMPEGCall.PATH_TOOL + source), (FFMPEGCall.PATH_GEN_RELATIVE + target), text, color, size).toString();
+    }
+    this.launchFfmpegCmd(ffmpegCmd);
+  }
+  
+  public void generateGif(final String source, final String target, final int t, final int w, final int l) {
+    Process p = null;
+    int time = 0;
+    int width = 320;
+    int length = 240;
+    if ((t != (-1))) {
+      time = t;
+    }
+    if ((w != (-1))) {
+      width = w;
+    }
+    if ((l != (-1))) {
+      length = l;
+    }
+    String ffmpegCmd = this.ffmpegVideoToGif(((FFMPEGCall.PATH_TOOL + FFMPEGCall.PATH_GEN_RELATIVE) + source), ((FFMPEGCall.PATH_TOOL + FFMPEGCall.PATH_GEN_RELATIVE) + target), time, width, length).toString();
+    this.launchFfmpegCmd(ffmpegCmd);
+  }
+  
+  public int videoDuration(final String locationVideo) {
+    try {
+      Process p = null;
+      String ffmpegCmd = this.ffmpegComputeDuration(locationVideo).toString();
+      p = Runtime.getRuntime().exec(ffmpegCmd);
+      p.waitFor();
+      InputStream _inputStream = p.getInputStream();
+      InputStreamReader _inputStreamReader = new InputStreamReader(_inputStream);
+      BufferedReader reader = new BufferedReader(_inputStreamReader);
+      String line = "";
+      String lines = "";
+      while ((!Objects.equal((line = reader.readLine()), null))) {
+        lines = (lines + line);
+      }
+      int _round = Math.round(Float.parseFloat(lines));
+      return (_round - 1);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  public void generateThumbnail(final int id, final String loc) {
+    String ffmpegCmd = this.ffmpegThumbnail(id, loc).toString();
+    this.launchFfmpegCmd(ffmpegCmd);
+  }
+  
+  public CharSequence ffmpegThumbnail(final int id, final String loc) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -i ");
+    _builder.append(loc);
+    _builder.append(" -ss 00:00:01.000 -vframes 1 ");
+    _builder.append((((FFMPEGCall.PATH_GEN_VIGNETTES_RELATIVE + this.tag) + "_") + Integer.valueOf(id)));
+    _builder.append(".jpg -y");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegFlipH(final String inputPath, final String outputPath) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -i ");
+    _builder.append(inputPath);
+    _builder.append(" -vf \"hflip\" ");
+    _builder.append(outputPath);
+    _builder.append(" -y");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegFlipV(final String inputPath, final String outputPath) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -i ");
+    _builder.append(inputPath);
+    _builder.append(" -vf \"vflip\" ");
+    _builder.append(outputPath);
+    _builder.append(" -y");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegNegate(final String inputPath, final String outputPath) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -i ");
+    _builder.append(inputPath);
+    _builder.append(" -vf \"negate\" ");
+    _builder.append(outputPath);
+    _builder.append(" -y");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegBN(final String inputPath, final String outputPath) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -i ");
+    _builder.append(inputPath);
+    _builder.append(" -vf \"hue=s=0\" ");
+    _builder.append(outputPath);
+    _builder.append(" -y");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegDrawTextTOP(final String inputPath, final String outputPath, final String text, final String color, final int size) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -i ");
+    _builder.append(inputPath);
+    _builder.append(" -vf \"drawtext=fontfile=\'C\\:\\\\Windows\\\\fonts\\\\Arial.ttf\':text=");
+    _builder.append(text);
+    _builder.append(":x=(w-text_w)/2:fontsize=");
+    _builder.append(size);
+    _builder.append(":fontcolor=");
+    _builder.append(color);
+    _builder.append("\" ");
+    _builder.append(outputPath);
+    _builder.append(" -y");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegDrawTextCENTER(final String inputPath, final String outputPath, final String text, final String color, final int size) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -i ");
+    _builder.append(inputPath);
+    _builder.append(" -vf \"drawtext=fontfile=\'C\\:\\\\Windows\\\\fonts\\\\Arial.ttf\':text=");
+    _builder.append(text);
+    _builder.append(":x=(w-text_w)/2:y=(h-text_h)/2:fontsize=");
+    _builder.append(size);
+    _builder.append(":fontcolor=");
+    _builder.append(color);
+    _builder.append("\" ");
+    _builder.append(outputPath);
+    _builder.append(" -y");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegDrawTextBOTTOM(final String inputPath, final String outputPath, final String text, final String color, final int size) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -i ");
+    _builder.append(inputPath);
+    _builder.append(" -vf \"drawtext=fontfile=\'C\\:\\\\Windows\\\\fonts\\\\Arial.ttf\':text=");
+    _builder.append(text);
+    _builder.append(":x=(w-text_w)/2:y=(h-text_h):fontsize=");
+    _builder.append(size);
+    _builder.append(":fontcolor=");
+    _builder.append(color);
+    _builder.append("\" ");
+    _builder.append(outputPath);
+    _builder.append(" -y");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegConcatenateCommand(final String mpegPlaylistFile, final String outputPath) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffmpeg -y -f concat -safe 0 -i ");
+    _builder.append(mpegPlaylistFile);
+    _builder.append(" -c copy -r 24 ");
+    _builder.append(outputPath);
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence ffmpegComputeDuration(final String locationVideo) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ");
+    _builder.append(locationVideo);
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public String ffmpegVideoToGif(final String videoLocation, final String outputPath, final int time, final int w, final int l) {
+    if ((time != 0)) {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("ffmpeg -i ");
+      _builder.append(videoLocation);
+      _builder.append(" -y -ss 0 -pix_fmt rgb8 -r 10 -t ");
+      _builder.append(time);
+      _builder.append(" -s ");
+      _builder.append(w);
+      _builder.append("x");
+      _builder.append(l);
+      _builder.append("  ");
+      _builder.append(outputPath);
+      _builder.newLineIfNotEmpty();
+      return _builder.toString();
+    } else {
+      StringConcatenation _builder_1 = new StringConcatenation();
+      _builder_1.append("ffmpeg -i ");
+      _builder_1.append(videoLocation);
+      _builder_1.append(" -y -ss 0 -pix_fmt rgb8 -r 10 -s ");
+      _builder_1.append(w);
+      _builder_1.append("x");
+      _builder_1.append(l);
+      _builder_1.append("  ");
+      _builder_1.append(outputPath);
+      _builder_1.newLineIfNotEmpty();
+      return _builder_1.toString();
+    }
+  }
+}
